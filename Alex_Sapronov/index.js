43730c80108864de8b98c093eeb54263f2fd228c
SapronovAS <suicider.deadmeat@live.ru>
2017-02-03 21:23:22 +0200

    new scrips wish Jesus

--- Alex_Sapronov/index.js
+++ Alex_Sapronov/index.js
@@ -1,11 +1,10 @@
 var test = function()
 {   
-   var text1 = 'Text one, ';
-   var text2 = 'Text two, ';
-   var text3 = 'Text tree, ';
-   var text4 = 'Text fore, ';
-   var number1 = 45;
-   var number2 = 34;
-   console.log (text1+text2+text3+text4);
-   console.log (number1-number2);
+   var name, old;
+   name = prompt('Yore name?');
+   old = prompt('How old are you?');
+   if (name=='Jesus'&& old == 33){
+console.log('Congratulation!! You are God');
+     }
+   else console.log ('Get out of there!!');
 }
