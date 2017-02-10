7130d9e07e6f0ea2b11d6e9fe2b8060edb9d90e8
nataliia-p <ponikarovska.nataliia@gmail.com>
2017-02-10 13:28:05 +0200

    Add files via upload

---
+++ ponikarovska_nataliia/Juses/index.js
@@ -0,0 +1,10 @@
+//declare variables for prompt
+var name = "Jesus";
+var age = "33";
+
+//ask user's name and user's age
+if(prompt("What is your name?") == name && (prompt("How old are you?") == age)){
+
+	//if answer is right
+	console.log("Welcome!");
+} 
