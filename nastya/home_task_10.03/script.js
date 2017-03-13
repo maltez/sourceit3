3a295201aea95ab5e6e19c03241be790c3956438
belikovanastasya <belikovaava@gmail.com>
2017-03-13 11:05:09 +0200

    add home task 10.03 whith comments

--- nastya/home_task_10.03/script.js
+++ nastya/home_task_10.03/script.js
@@ -1,9 +1,13 @@
+
+/* Declaration Function constructor*/
+
 function Tank(fuel,cartriges){
 	this.quantityFuel = fuel;
 	document.getElementById('Fuel-balance').value = this.quantityFuel;
 	this.quantityCartriges = cartriges;
 	document.getElementById('Cartrige-balance').value = this.quantityCartriges;
-	this.move = function (){
+	this.crew = 0;
+	this.move = function (){                // object method 'move' wich reduces the rest of fuel
 		if(this.quantityFuel ==0){
 			alert("Tank is not move");
 		}
@@ -15,7 +19,7 @@ function Tank(fuel,cartriges){
 	 
 	}
 
-	this.fire = function (){
+	this.fire = function (){				//object method which reduce the rest of cartriges
 	if(this.quantityCartriges ==0){
 			alert("There are no cartriges");
 		}
@@ -23,7 +27,7 @@ function Tank(fuel,cartriges){
 			this.quantityCartriges = this.quantityCartriges - 1;
 		}
 	} 
-	this.refill = function (fuel,cartriges){
+	this.refill = function (fuel,cartriges){			// object method wich replenishes balance of fuel and cartriges
 	this.quantityFuel = this.quantityFuel+fuel;
 	this.quantityCartriges = this.quantityCartriges+cartriges;
 }
@@ -31,19 +35,19 @@ function Tank(fuel,cartriges){
 }
 
 
-var battleTank = new Tank(5,5);
+var battleTank = new Tank(5,5);  //instance of class Tank
 
 document.getElementById('move').onclick = function(){
-	battleTank.move();
+	battleTank.move();  // call method 'move'
 return document.getElementById('Fuel-balance').value = battleTank.quantityFuel;
 }
 document.getElementById('fire').onclick = function(){
-	battleTank.fire();
+	battleTank.fire(); //call method 'fire'
 return document.getElementById('Cartrige-balance').value = battleTank.quantityCartriges;
 }
 
 
-document.getElementById('refill').onclick = function(){
+document.getElementById('refill').onclick = function(){ // call method 'refill'
 	var fuel = +(document.getElementById('Fuel-refill').value);
 var cartriges = +(document.getElementById('Cartrige-refill').value);
 	
