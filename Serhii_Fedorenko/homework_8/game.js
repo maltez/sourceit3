6079b275f10c84e09df4badf019f13a9885a5fde
Spayn <dr.spayn@yandex.ua>
2017-03-20 11:53:15 +0200

    update Tanks game

--- Serhii_Fedorenko/homework_8/game.js
+++ Serhii_Fedorenko/homework_8/game.js
@@ -1,113 +1,77 @@
-// craete game map
-var prokhorovka = new PlayingField();
-
-    function createTank(){
-        var t64 = new Tank(100, 10);
-        prokhorovka.addTank(t64);
-    }
-    
-    createTank();
-
-    var tanks = prokhorovka.getTanks();
-
-// in EventListener
-function gameTanks(e){
-     
-    var playingTank = document.getElementById("playingTank");
-    var actualFuel = document.getElementById("fuel");
-    var actualCartridges = document.getElementById("cartridges");
-    // Get the style for playingTank
-    var cs = window.getComputedStyle(playingTank);
-     
-    var left = parseInt(cs.marginLeft);
-    var top = parseInt(cs.marginTop);
-
-    switch(e.keyCode){
-        
-        case 32:   // space key
-                tanks.forEach(function(tank) {
-                    tank.fire();
-                       if (tank.cartridges > 1){
-                           playingTank.style.backgroundImage = "none";
-                           setTimeout(function() { playingTank.style.backgroundImage = "url(\"./tank.jpg\")"; }, 50);
-                       };
-                  });
-            break;
-        case 37:  // left key
-            if(left > 0)
-                tanks.forEach(function(tank) {
-                    tank.moove();
-                        if (tank.fuel > 1){
-                            playingTank.style.marginLeft = left - 10 + "px";
-                        }
-                });
-            break;
-        case 38:   // up key
-            if(top > 0)
-                tanks.forEach(function(tank) {
-                    tank.moove();
-                        if (tank.fuel > 1){
-                            playingTank.style.marginTop = top - 10 + "px";
-                        }
-                });
-            break;
-        case 39:   // right key
-            if(left < 450)
-                tanks.forEach(function(tank) {
-                    tank.moove();
-                        if (tank.fuel > 1){
-                            playingTank.style.marginLeft = left + 10 + "px";
-                        }
-                });
-            break;
-        case 40:   // down key
-            if(top < 450)
-                tanks.forEach(function(tank) {
-                    tank.moove();
-                        if (tank.fuel > 1){
-                            playingTank.style.marginTop = top + 10 + "px";
-                        }
-                });
-            break;
-    }
-
-// for game interface 
-    tanks.forEach(function(tank) {
-                    actualFuel.innerHTML = tank.fuel;
-                  });
-    tanks.forEach(function(tank) {
-                    actualCartridges.innerHTML = tank.cartridges;
-                  });  
-}
- 
-addEventListener("keydown", gameTanks);
-
 // constructor
 function Tank(fuel, cartridges) {
     this.fuel = fuel;
     this.cartridges = cartridges;
     this.crewmembers = []; // For the crew
+    this.playingTank = document.getElementById("playingTank");
+    this.actualFuel = document.getElementById("fuel");
+    this.actualCartridges = document.getElementById("cartridges");
+    this.actualFuel.innerHTML = this.fuel;
+    this.actualCartridges.innerHTML = this.cartridges;
 }
 
-    Tank.prototype.moove = function() {
+    Tank.prototype.engine = function() {
+        this.fuel--;
+    };
+    
+    Tank.prototype.gun = function() {
+        this.cartridges--;
+    };
+
+    Tank.prototype.moove = function(keyCode) {
+        // Get the style for playingTank
+        var cs = window.getComputedStyle(playingTank);
+        this.left = parseInt(cs.marginLeft);
+        this.top = parseInt(cs.marginTop);
+        
         if (this.fuel < 1){
             console.log('The fuel ran out!');
         } else {
-            this.fuel--;
+            switch(keyCode){
+                case 37:  // left key
+                    if(this.left > 0)
+                            this.playingTank.style.marginLeft = this.left - 10 + "px";
+                            this.engine();
+                    break;
+                case 38:   // up key
+                    if(this.top > 0)
+                            this.playingTank.style.marginTop = this.top - 10 + "px";
+                            this.engine();
+                    break;
+                case 39:   // right key
+                    if(this.left < 450)
+                            this.playingTank.style.marginLeft = this.left + 10 + "px";
+                            this.engine();
+                    break;
+                case 40:   // down key
+                    if(this.top < 450)
+                            this.playingTank.style.marginTop = this.top + 10 + "px";
+                            this.engine();
+                    break;
+            }
         }
+        
+        this.actualFuel.innerHTML = this.fuel;
     };
 
     Tank.prototype.fire = function() {
+        var me = this;
         if (this.cartridges < 1){
             console.log('The cartridges are over!');
-        } else {
-            this.cartridges--;
+        } else { 
+            me.playingTank.style.backgroundImage = "none";
+            setTimeout(function() { me.playingTank.style.backgroundImage = "url(\"./tank.jpg\")"; }, 50);
+            this.gun();
         }
+        
+        this.actualCartridges.innerHTML = this.cartridges;
     };
   
-    Tank.prototype.refuel = function(fuel, cartridges) {
+    Tank.prototype.recharge = function(fuel, cartridges) {
         this.fuel += fuel;
         this.cartridges += cartridges;
+        this.actualFuel.innerHTML = this.fuel;
+        this.actualCartridges.innerHTML = this.cartridges;
     };
     
     Tank.prototype.setCrow = function(crewmember) {
@@ -115,29 +79,21 @@ function Tank(fuel, cartridges) {
     };
 
 // constructor
-function ModernizedTank() {
+function ModernizedTank(fuel, cartridges) {
     // Functional-based inheritance of the constructor Tank
-    Tank.apply(this);
+    Tank.apply(this, [fuel, cartridges]);
 }
 
 // Prototype-based inheritance
 ModernizedTank.prototype = Object.create(Tank.prototype);
 ModernizedTank.prototype.constructor = ModernizedTank;
 
-    ModernizedTank.prototype.fire = function() {
-        if (this.cartridges < 5){
-            console.log('The cartridges are over!');
-        } else {
-            this.cartridges -= 5;
-        }
+    ModernizedTank.prototype.engine = function() {
+        this.fuel -= 2;
     };
-  
-    ModernizedTank.prototype.moove = function() {
-        if (this.fuel < 2){
-            console.log('The fuel ran out!');
-        } else {
-            this.fuel -= 2;
-        }
+    
+    ModernizedTank.prototype.gun = function() {
+        this.cartridges -= 5;
     };
 
 function PlayingField() {
@@ -155,3 +111,47 @@ function PlayingField() {
             return this.arrTank;
         };
 }
+
+// craete game map
+var prokhorovka = new PlayingField();
+
+var t64 = new Tank(100, 10);
+//var t64b = new ModernizedTank(100, 10);   // for the test
+prokhorovka.addTank(t64);
+//prokhorovka.addTank(t64b);    // for the test
+var tanks = prokhorovka.getTanks();
+        
+window.onkeydown = function (e) {
+    switch(e.keyCode){
+        case 32:  // space key
+            tanks.forEach(function(tank) {
+                tank.fire();
+            });
+            break;
+        case 37:  // left key
+            tanks.forEach(function(tank) {
+                tank.moove(e.keyCode);
+            });
+            break;
+        case 38:   // up key
+            tanks.forEach(function(tank) {
+                tank.moove(e.keyCode);
+            });
+            break;
+        case 39:   // right key
+            tanks.forEach(function(tank) {
+                tank.moove(e.keyCode);
+            });
+            break;
+        case 40:   // down key
+            tanks.forEach(function(tank) {
+                tank.moove(e.keyCode);
+            });
+            break;
+        case 82:   // R key
+            tanks.forEach(function(tank) {
+                tank.recharge(100, 10);
+            });
+            break;
+    }
+};
