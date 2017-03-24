8184456802f9322982fc147136e9432a0129fb67
belikovanastasya <belikovaava@gmail.com>
2017-03-24 17:02:08 +0200

    Add home task_20.03

---
+++ nastya/home_task_20.03/script.js
@@ -0,0 +1,209 @@
+/* Declaration Function constructor*/
+
+class Tank {
+
+	constructor(fuel, cartriges) {
+			this.quantityFuel = fuel;
+			document.getElementById('fuel-balance').value = this.quantityFuel;
+			this.fuelConsumption = 1;
+			this.cartrigesConsumption = 1;
+			this.quantityCartriges = cartriges;
+			document.getElementById('cartrige-balance').value = this.quantityCartriges;
+			this.tankObj = document.getElementById('tank');
+			// this.id = 'tank'+ Math.floor(Math.random()*10);
+			this.crew = 4;
+
+		}
+		/*Write the object's methods to the object's prototype*/
+	move(keyCode) {
+		let CurrentPos = window.getComputedStyle(this.tankObj); // determine the current position of tank;
+		this.left = parseInt(CurrentPos.left);
+		this.top = parseInt(CurrentPos.top);
+
+
+		if (this.quantityFuel == 0) {
+			alert("The Tank does not move");
+		} else {
+			this.quantityFuel = this.quantityFuel - this.fuelConsumption;
+			switch (keyCode) {
+				case 37: // left
+					if (this.left > 0)
+						this.tankObj.style.left = this.left - 20 + 'px';
+
+
+					break;
+				case 38: // up
+					if (this.top > 0)
+						this.tankObj.style.top = this.top - 20 + 'px';
+					break;
+				case 39: // right
+
+					if (this.left < 500)
+						this.tankObj.style.left = this.left + 20 + 'px';
+					break;
+				case 40: // down
+					if (this.top < 500)
+						this.tankObj.style.top = this.top + 20 + 'px';
+					break;
+			}
+
+		}
+
+	}
+
+
+	/*fire method*/
+	fire() {
+		let self = this;
+		if (this.quantityCartriges == 0) {
+			alert("There are no cartriges");
+		} else {
+			this.quantityCartriges = this.quantityCartriges - this.cartrigesConsumption;
+
+			this.tankObj.style.backgroundColor = 'red'; //change background on space click
+			var bg = function () {
+				self.tankObj.style.backgroundColor = "#eeeeee"
+			}
+			setTimeout(bg, 50); // change background back through time
+
+		}
+	}
+	refill(fuel, cartriges) {
+		this.quantityFuel = this.quantityFuel + fuel;
+		this.quantityCartriges = this.quantityCartriges + cartriges;
+	}
+}
+
+/* Declaration Function constructor for new Tank,
+which inherits the properties and methods of the tank object */
+class FiveBattleTank extends Tank {
+	constructor(fuel, cartriges) {
+		super(fuel, cartriges);
+		this.fuelConsumption = 2;
+		this.cartrigesConsumption = 5;
+	}
+
+}
+// extends new class of class Tank
+class Enemy extends Tank {
+	constructor(fuel, cartriges, id) {
+		super(fuel, cartriges);
+		this.id = 'enemy' + id;
+	}
+
+	move() {
+		this.enemyObj = document.getElementById(this.id);
+
+		let self = this;
+		var CurrentPos = window.getComputedStyle(self.enemyObj);
+		setInterval(forward, 100);
+
+
+		//Algorithm of tank movement
+		function back() {
+			self.left = parseInt(CurrentPos.left);
+			self.top = parseInt(CurrentPos.top);
+			if (self.top > 0 && self.left >= 0) {
+				self.enemyObj.style.left = self.left - 5 + "px";
+				self.enemyObj.style.top = self.top - 5 + "px";
+			} else {
+				setInterval(forward, 100);
+			}
+		}
+
+		function forward() {
+			// determine the current position of tank;
+			self.left = parseInt(CurrentPos.left);
+			self.top = parseInt(CurrentPos.top);
+			if (self.top < 300 && self.left < 400) {
+				self.enemyObj.style.left = self.left + 5 + "px";
+				self.enemyObj.style.top = self.top + 5 + "px";
+			} else {
+				setInterval(back, 100);
+			}
+		}
+	}
+
+}
+
+
+/* Declaration Function constructor*/
+class Playground {
+	constructor() {
+			this.arrTanks = []; //array of tanks on playground
+		}
+		/*Write the object's methods to the object's prototype*/
+	addTanks(newTank) {
+		if (this.arrTanks.length < 3) {
+
+			this.arrTanks.push(newTank);
+			let parentNode = document.getElementById('playground')
+			let tankFigure = document.createElement("div");
+			parentNode.appendChild(tankFigure);
+			tankFigure.className = 'tank'
+			tankFigure.id = newTank.id;
+			tankFigure.style.top = Math.floor(Math.random() * 500) + 'px';
+			tankFigure.style.left = Math.floor(Math.random() * 500) + 'px';
+
+
+		} else {
+			throw new Error("You should put on playground no more than 4 tanks");
+
+		}
+	}
+}
+
+
+let battleTank = new Tank(25, 25);
+/*declaration function for control tank*/
+let moveTankOnKey = function (e) {
+	switch (e.keyCode) {
+		case 32: // space key
+
+			battleTank.fire();
+
+			break;
+		case 37: // left key
+			battleTank.move(e.keyCode);
+
+			break;
+		case 38: // up key
+			battleTank.move(e.keyCode);
+			break;
+		case 39: // right key
+			battleTank.move(e.keyCode);
+
+			break;
+		case 40: // down key
+			battleTank.move(e.keyCode);
+			break;
+
+	}
+};
+
+/*declaration function for statistic*/
+let statOnKey = function () {
+	return [document.getElementById('fuel-balance').value = battleTank.quantityFuel,
+		document.getElementById('cartrige-balance').value = battleTank.quantityCartriges
+	]
+}
+
+/*Assign event handlers*/
+window.addEventListener('keydown', moveTankOnKey);
+window.addEventListener('keydown', statOnKey);
+
+refill.onclick = function () {
+
+	// call method 'refill'
+	battleTank.refill(10, 10);
+	return [document.getElementById('fuel-balance').value = battleTank.quantityFuel,
+		document.getElementById('cartrige-balance').value = battleTank.quantityCartriges
+	];
+}
+let playground = new Playground();
+let i = 0;
+document.getElementById('Enemy').onclick = function () {
+	let enemy = new Enemy(5, 5, i++);
+	playground.addTanks(enemy);
+	enemy.move();
+}
