07d5f99175b31b0aa6152ebe273b9e74576180fe
belikovanastasya <belikovaava@gmail.com>
2017-04-04 15:10:11 +0300

    add home task 31.03 with changes for the correct work

--- nastya/home_task_31.03/script.js
+++ nastya/home_task_31.03/script.js
@@ -1,25 +1,34 @@
-var promise = function(i){
-	return new Promise(
-		function(resolve, reject){
-   			setTimeout(function(){
-    		console.log(i);
-   			}, i*1000)
-  
-  })
+'use strict'
+/*decision with promises method
+ */
+function promiseMethodCode() {
+	var promise = function (i) {
+		return new Promise(
+			function (resolve, reject) {
+				setTimeout(function () {
+					console.log(i);
+				}, i * 1000)
+				resolve()
+			})
+	}
+	for (var i = 0; i < 4; i++) {
+		promise(i);
+
+	}
 }
-for (var i = 0; i < 4; i++ ){
-       promise(i)
 
+/*decision with bind method
+ */
+
+function bindMethodCode() {
+	for (var j = 0; j < 4; j++) {
+
+
+		setTimeout(fun.bind(null, j)(), 3000)
+	}
+
+	function fun(j) {
+		console.log(j);
+	}
 }
 
-
-
-
-for (var j = 0; j<4; j++){
-
-
-    setTimeout(fun.bind(null, j), 3000)}
-
-function fun(j){
-    console.log (j)
-}
